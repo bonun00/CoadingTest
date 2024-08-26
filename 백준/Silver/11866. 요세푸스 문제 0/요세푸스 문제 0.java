7a468f5fa7389a1c6c8b28726d752309a0dc9caf
bonun00 <lifesciences00@naver.com>
2024-08-26 17:12:04 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,36 +1,32 @@
-
-
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String command =reader.readLine();
-        int num=Integer.parseInt(command.split(" ")[0]);
-        int point=Integer.parseInt(command.split(" ")[1]);
-        LinkedList <Integer> result=new LinkedList<>();
-        LinkedList<Integer> list =new LinkedList<>();
-        int index=0;
-        for(int i=1; i<=num; i++){
-            list.add(i);
+        String input = reader.readLine();
+        int len = Integer.parseInt(input.split(" ")[0]);
+        int num = Integer.parseInt(input.split(" ")[1]);
+        int comm=0;
+        List arr =new ArrayList<>();
+        ArrayList result= new ArrayList<>();
+//        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=len; i ++){
+            arr.add(i);
         }
-        while(list.size()!=0){
-            index=(index+ point - 1)% list.size();
-            result.add(list.get(index ));
-            list.remove(index);
+        for(int i= 0; i<len; i++){
+            comm=(comm+ num-1)%arr. size();
+            result.add(arr.get(comm));
+            arr.remove(comm);
         }
-
         System.out.print("<");
-        for (int i =0; i< result.size()-1; i++){
-            System.out.print(result.get(i)+", ");
-        }
-        System.out.print(result.get(result.size()-1));
+        for(int i=0; i<len-1; i++){
+        System.out.print(result.get(i)+", ");}
+        System.out.print(result.get(len-1));
         System.out.print(">");
-
     }
 }
