3340858ff3c40020ae2a713238d5e6fef34659fd
bonun00 <lifesciences00@naver.com>
2024-08-26 17:03:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 144 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String command =reader.readLine();
+        int num=Integer.parseInt(command.split(" ")[0]);
+        int point=Integer.parseInt(command.split(" ")[1]);
+        LinkedList <Integer> result=new LinkedList<>();
+        LinkedList<Integer> list =new LinkedList<>();
+        int index=0;
+        for(int i=1; i<=num; i++){
+            list.add(i);
+        }
+        while(list.size()!=0){
+            index=(index+ point - 1)% list.size();
+            result.add(list.get(index ));
+            list.remove(index);
+        }
+
+        System.out.print("<");
+        for (int i =0; i< result.size()-1; i++){
+            System.out.print(result.get(i)+", ");
+        }
+        System.out.print(result.get(result.size()-1));
+        System.out.print(">");
+
+    }
+}
