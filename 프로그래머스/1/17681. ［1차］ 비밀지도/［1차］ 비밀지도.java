834c5d314d4c7cd32dc8e07b09fe89567d7ac0b1
구본훈 <lifesciences00@naver.com>
2025-08-21 10:21:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.59 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i =0; i<n; i++){
+           String f=String.format("%"+n+"s", 
+                                 Integer.toBinaryString(arr1[i] | arr2[i]))
+                    .replace(' ','0');
+            answer[i]=f.replace('1','#').replace('0', ' ');
+                       
+        }
+        return answer;
+    }
+}
