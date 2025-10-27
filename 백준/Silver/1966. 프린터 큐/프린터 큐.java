d1bf867d047748ac02d6dd7c9d4f86612f72789a
구본훈 <lifesciences00@naver.com>
2025-10-27 20:05:59 +0900

    [Silver III] Title: 프린터 큐, Time: 176 ms, Memory: 17132 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,60 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            int w = Integer.parseInt(tokenizer.nextToken());
+            int p= Integer.parseInt(tokenizer.nextToken());
+            Queue<Integer> queue = new LinkedList<>();
+            Queue<Integer> q = new LinkedList<>();
+            StringTokenizer tokenizer2 = new StringTokenizer(reader.readLine());
+            for(int j = 0; j < w; j++){
+                queue.add(Integer.parseInt(tokenizer2.nextToken()));
+                if(p==j){
+                    q.add(1);
+                }else{
+                    q.add(0);
+                }
+            }
+            int answer=0;
+            while(true){
+                int max=queue.stream().max(Integer::compare).get();
+                if(queue.peek()==max){
+                    queue.poll();
+                    answer++;
+                    int isTarget=q.poll();
+                    if(isTarget==1){
+                        break;
+                    }
+
+                }else{
+                    int a= queue.poll();
+                    int b= q.poll();
+                    queue.add(a);
+                    q.add(b);
+                }
+            }
+            System.out.println(answer);
+
+
+
+        }
+    }
+}
+
+
+
+
