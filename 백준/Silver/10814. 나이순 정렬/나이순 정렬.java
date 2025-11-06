59d01b0cacc4bcedba587781d3651d4af8f4dc84
구본훈 <lifesciences00@naver.com>
2025-11-06 23:10:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 1080 ms, Memory: 52296 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,60 +1,34 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
 
-public class Main {
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.ArrayList;
+    import java.util.Map;
+    import java.util.StringTokenizer;
+    import java.util.TreeMap;
+    import java.util.List;
 
+    public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        List<Integer> list = new ArrayList<>();
-        Set<Integer> same = new HashSet<>();
-        Map<Integer, String > map = new HashMap<>();
-        int num = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < num; i++) {
-            StringTokenizer st2 = new StringTokenizer(reader.readLine());
-            int a = Integer.parseInt(st2.nextToken());
-            String b = st2.nextToken();
-            if(!list.contains(a)){
-                list.add(a);
-            }
+        Map<Integer,List<String>> map = new TreeMap<>();
+        int n = Integer.parseInt(reader.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(reader.readLine());
 
-            if(map.containsKey(a)){
-                map.put(a,map.get(a)+" "+b);
-                same.add(a);
-            }else{
-                map.put(a,b);
-            }
-
+            map.computeIfAbsent(Integer.parseInt(st.nextToken()),k -> new ArrayList<>()).add(st.nextToken());
         }
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            if(same.contains(list.get(i))){
-                String[] str=map.get(list.get(i)).split(" ");
-                for(int j=0;j<str.length;j++){
-                    System.out.println(list.get(i)+" "+str[j]);
-                }
-                same.remove(list.get(i));
-            }else{
-                System.out.println(list.get(i)+" "+map.get(list.get(i)));
-            }
 
+        for(int num:map.keySet()){
+            int temp=map.get(num).size();
+            for(int i=0;i<temp;i++){
+                System.out.println(num+" "+map.get(num).get(i));
+            }
         }
+
+
+
 
 
     }
-
 }
-
-
-
