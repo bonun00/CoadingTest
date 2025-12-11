9852c510aabdf0cd96f5d66c802bc5a05db6d9ce
구본훈 <lifesciences00@naver.com>
2025-12-11 23:56:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 276 ms, Memory: 25244 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int aa = Math.abs(a);
+            int bb = Math.abs(b);
+            if (aa == bb) {
+                return a - b;
+            }
+            return aa - bb; 
+        });
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
