58a47e422de91c10e126383397b57c0766e7dccd
구본훈 <lifesciences00@naver.com>
2025-12-12 00:39:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 268 ms, Memory: 25884 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,38 +1,48 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        List<Integer> h = new LinkedList<>();
+        int min = Integer.MAX_VALUE;
 
-        StringBuilder sb = new StringBuilder();
+
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
             int aa = Math.abs(a);
             int bb = Math.abs(b);
             if (aa == bb) {
                 return a - b;
+            } else {
+                return aa -bb ;
             }
-            return aa - bb; 
+
         });
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-
-            if (x == 0) {
-                if (pq.isEmpty()) {
-                    sb.append("0\n");
-                } else {
-                    sb.append(pq.poll()).append('\n');
-                }
-            } else {
-                pq.add(x);
+            int temp = Integer.parseInt(br.readLine());
+            if (temp != 0) {
+                pq.add(temp);
             }
-        }
+            if (temp == 0 && pq.isEmpty()) {
+                sb.append(0).append("\n");
+            } else if (temp == 0) {
+                sb.append(pq.poll()).append("\n");
+            }
 
-        System.out.print(sb.toString());
+        }
+        System.out.println(sb);
     }
 }
+
+
+
+
+
