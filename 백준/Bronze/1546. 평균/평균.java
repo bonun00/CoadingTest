2812cd783dfa54d4b5accdae0e17465a0deafcc9
구본훈 <lifesciences00@naver.com>
2025-12-15 00:09:54 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,45 @@
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        double[] num = new double[N];
-        double max = 0;
-
+        double[] num=new double[N];
+        double max=Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
-            num[i] = Double.parseDouble(st.nextToken());
-            if (num[i] > max) max = num[i];
+            num[i] = Integer.parseInt(st.nextToken());
+            if(max<num[i]){
+                max=num[i];
+            }
         }
+        double sum=0;
 
-        double sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += (num[i] / max) * 100.0;
+
+        for(int i=0;i<N;i++){
+           if(num[i]!=max){
+               sum+=(num[i]/max*100.0);
+
+           }else{
+               sum+=100;
+           }
         }
+        System.out.println(sum/N);
 
-        System.out.println(sum / N);
+
+
     }
 }
+
+
+
+
+
+
+
