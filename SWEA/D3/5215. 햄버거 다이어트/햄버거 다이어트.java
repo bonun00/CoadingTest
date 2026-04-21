edc07a57ec526c36e25f43f04441cae85f088ce2
구본훈 <lifesciences00@naver.com>
2026-04-21 18:29:55 +0900

    [D3] Title: 햄버거 다이어트, Time: 177 ms, Memory: 30,704 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+    
+    static int[][] ham;
+    static int ans;
+    static int n;
+    static int limit;
+    static boolean[] visited;
+    
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			n=sc.nextInt();
+            limit=sc.nextInt();
+            ham=new int[n][2];
+            for(int i=0; i<n; i++){
+                int s=sc.nextInt();
+                int c=sc.nextInt();
+                ham[i][0]=s;
+                ham[i][1]=c;
+            }
+            ans=0;
+    
+            dfs(0,0,0);
+            
+			System.out.println("#"+test_case+" "+ans);
+		}
+	}
+    public static void dfs(int index, int score,int cal){
+        	if(cal>limit) return;
+        
+    
+            	if(score>ans){
+                	ans=score;
+                }
+         
+           if(index==n)return;
+      
+        
+           dfs(index+1, score+ham[index][0],cal+ham[index][1]);
+           dfs(index+1, score,cal);
+    } 
+}
