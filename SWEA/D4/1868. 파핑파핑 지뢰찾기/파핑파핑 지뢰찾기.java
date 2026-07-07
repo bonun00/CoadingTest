506fbfc90d018064ec71bf55ecc59641ba6198d4
구본훈 <lifesciences00@naver.com>
2026-07-07 23:22:26 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 261 ms, Memory: 49,020 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,89 +1,131 @@
-import java.util.*;
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = &apos;b&apos;;
+// String var = &quot;ABCDEFG&quot;;
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
 import java.io.FileInputStream;
 
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
     static int n;
-    static char[][] bom;
+    static char[][] b;
     static boolean[][] visited;
-    static int [][] move={{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
+    
 	public static void main(String args[]) throws Exception
 	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream(&quot;res/input.txt&quot;));
 
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			n=sc.nextInt();
-			bom=new char[n][n];            
-            
-            for(int i=0; i<n; i++){
-    			bom[i]=sc.next().toCharArray();              
-            }
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
- 					if(bom[i][j]=='.'){
-                    	find(i,j);                
-                    }                             	
-                }
-            }
-            visited=new boolean[n][n];
+            b=new char[n][n];
             int ans=0;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
- 					if(bom[i][j]=='0'&&!visited[i][j]){
-                    	bfs(i,j);                
-                		ans++;
-                    }                             	
-                }
-            }
-     
-			for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
- 					if(bom[i][j]!='*'&&!visited[i][j]){
-                 		ans++;           
-                    }                             	
-                }
-            }
-            System.out.println("#"+test_case+' '+ans);
             
+            for(int i=0; i<n; i++){
+                String temp=sc.next();
+            	for(int j=0; j<n; j++){
+            		b[i][j]=temp.charAt(j);
+            	}
+            }
+			visited=new boolean[n][n];
+            
+            for(int i=0; i<n; i++){
+            	for(int j=0; j<n; j++){
+                    if(b[i][j]=='.'&&!visited[i][j]&&zero(i,j)) {
+						dfs(i,j);
+                        ans++;
+                    }
+            	}
+            }
+            for(int i=0; i<n; i++){
+            	for(int j=0; j<n; j++){
+                    if(b[i][j]=='.'&&!visited[i][j]) {
+                        ans++;
+                    }
+            	}
+            }
+            
+            
+			System.out.println("#"+test_case+" "+ans);
 		}
 	}
-    static void find(int a, int b){
- 
- 		int res=0;
-		for(int i=0; i<8; i++){
-            int ma=a+move[i][0];
-            int mb=b+move[i][1];   
- 			if(ma<0||mb<0||ma>=n||mb>=n) continue;
-			if(bom[ma][mb]=='*')res++;            
+    static boolean zero(int x,int y){
+        	int[][] m={{1,0},{0,1},{0,-1},{-1,0},{1,1},{-1,1},{1,-1},{-1,-1}};
+        boolean bool=true;
+    	 for(int i=0; i<8; i++){
+        	int mx=x+m[i][0];
+            int my=y+m[i][1];
+            if(mx<0||my<0||mx>=n||my>=n)continue;
+            if(b[mx][my]=='*') bool=false;
         }
-		bom[a][b]=(char)(res+'0');
-    
+        return bool;
     }
-
-    static void bfs(int a, int b ){
-         Queue<int[]> q=new ArrayDeque<>();
-     	 q.add(new int[]{a,b});		
-	      visited[a][b]=true;
-        while(!q.isEmpty()){
-        	int[] temp=q.poll();
-		
-	        for(int i=0; i<8; i++){
-               	int ma=temp[0]+move[i][0];
-                int mb=temp[1]+move[i][1];                 
-              	if(ma<0||mb<0||ma>=n||mb>=n) continue;
-                if(visited[ma][mb])continue;
-                if(bom[temp[0]][temp[1]]=='0'){
-                    visited[ma][mb]=true;
-                 	q.add(new int[]{ma,mb});
-                }
-            }
+    
+    
+    static void dfs(int x, int y){
+    	int[][] m={{1,0},{0,1},{0,-1},{-1,0},{1,1},{-1,1},{1,-1},{-1,-1}};
+        visited[x][y]=true;
+        int cnt=0;
+        for(int i=0; i<8; i++){
+        	int mx=x+m[i][0];
+            int my=y+m[i][1];
+            if(mx<0||my<0||mx>=n||my>=n)continue;
+            if(b[mx][my]=='*')cnt++;
+        }
+ 		if(cnt==0){
+        	for(int i=0; i<8; i++){
+        	int mx=x+m[i][0];
+            int my=y+m[i][1];
+            if(mx<0||my<0||mx>=n||my>=n||visited[mx][my])continue;
+            dfs(mx,my);
+        }
+            
         }
         
-    
+        
     }
-    
 }
