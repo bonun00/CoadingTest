0c6ab1104664bd4ca2f386fe19d56352955237f8
구본훈 <lifesciences00@naver.com>
2026-09-02 11:49:32 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 119 ms, Memory: 30,136 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,63 +1,93 @@
-
-import java.util.Scanner;
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
 import java.io.FileInputStream;
 
-
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
-    static int[][] miro;
-    static int[] start;
-    static int ans;
-    static boolean[][] visited;
-    static int[][] move={{1,0},{0,1},{-1,0},{0,-1}};
 	public static void main(String args[]) throws Exception
 	{
+
 		Scanner sc = new Scanner(System.in);
-		int T;
+
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
-			T=sc.nextInt();
-            start=new int[2];
-            miro=new int [16][16]; 
-            visited=new boolean[16][16];
-            ans=0;
+			int t=sc.nextInt();
+            int x=0;
+             int y=0;
+            int[][] miro=new int[16][16];
             for(int i=0; i<16; i++){
-            	String[] str =sc.next().split("");
-                for(int j=0; j<16; j++){
-                    miro[i][j]=Integer.parseInt(str[j]);
+                String[] str=sc.next().split("");
+            	for(int j=0; j<16; j++){
+                	miro[i][j]=Integer.parseInt(str[j]);
                     if(miro[i][j]==2){
-                    	start[0]=i;
-                        start[1]=j;
+                    	x=i;
+                        y=j;
                     }
                 }
             }
+            boolean ans=bfs(x,y,miro);
+            System.out.println("#"+t+" "+(ans?1:0));
             
-            
-            dfs(start);
-            
-            System.out.println("#"+test_case+" "+ans);
-           
-            
-            
-
+				
 		}
 	}
+    static boolean bfs(int a, int b, int[][] miro){
+    	ArrayDeque<int[]> q=new ArrayDeque<>();
+        boolean[][] visited=new boolean[16][16];
+        int[][] move={{1,0},{0,1},{0,-1},{-1,0}};
+        
+        q.add(new int[]{a,b});
+        visited[a][b]=true;
+        while(!q.isEmpty()){
+        	int[] t=q.poll();
+            if(miro[t[0]][t[1]]==3)return true;
+            
+            for(int i=0; i<4; i++){
+            	int ma=t[0]+move[i][0];
+                int mb=t[1]+move[i][1];
+                if(ma<0||mb<0||ma>=16||mb>=16)continue;
+                if(miro[ma][mb]==1||visited[ma][mb])continue;
+                visited[ma][mb]=true;
+                q.add(new int[]{ma,mb});
+            }	
+        
+        }
+        return false;
+        
+        
+        
     
-    public static void dfs(int[] s){
-        int x=s[0];
-        int y=s[1];
-    	if(miro[x][y]==1) return;
-        if(visited[x][y])return;
-        visited[x][y]=true;
-		if(miro[x][y]==3){
-        	ans=1;
-            return;
-        }
-        for(int i=0; i<4; i++){
-        	int mx=x+move[i][0];
-            int my=y+move[i][1];
-            if(0>mx|| 0>my||16<=mx||16<=my)continue;
-            dfs(new int[]{mx, my});
-        }
+    
     }
+    
 }
